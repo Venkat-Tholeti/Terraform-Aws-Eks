092054a6df8af288612d77014266211ac29a9b0c
venkat <devopsaws84s@gmail.com>
2026-03-19 12:45:30 +0530

    JENKINS

--- Acm/main.tf
+++ Acm/main.tf
@@ -16,7 +16,7 @@ resource "aws_acm_certificate" "daws84s" {
 
 resource "aws_route53_record" "devopsaws" {
   for_each = {
-    for dvo in aws_acm_certificate.daws84s.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.devopsaws.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
